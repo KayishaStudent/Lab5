5d9057687e5356f798ec3cde1bc9be398ecd9c78
KayishaStudent <2562592@students.wits.ac.za>
2025-03-17 16:59:01 +0200

    More routes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express()
 app.use(express.json())
-const books = [];
+let books = [];
 
 const PORT= process.env.PORT||3000;
 
@@ -19,30 +19,102 @@ app.get("/whoami", (request, response) => {
 });
 
 app.get("/books", (request, response) => {
-      
-   response.send(books);
+   
+      response.send(books);
 });
 
-id = 500;
+
 app.get("/books/:id", (request, response) => {
-   try {
-      const bookId = request.params.id;
-      response.status(200).send(books[books.id]); 
+   console.log(`Request to find book with ${request.params.id}`)
+   books.forEach(b => {
+      if (b.id === request.params.id) {
+         console.log("FOUBND");
+         console.log(`${b.title}`);
+         response.send(b);
+      }
+   })
       
-   }
-   catch {
-      const error={  "error": "Missing required book details"}
-      response.status(404).send(error);
-    }
-   
+ 
 });
+
 
 app.post("/books", (request, response) => {
    const body = request.body;
-   books.push(body);
+   if (!request.body.id) {
+      response.status(400).send("400 Bad request");
+   }
+   else if (!request.body.title) {
+      response.status(400).send("400 Bad request");
+   }
+   else if (!request.body.details) {
+      response.status(400).send("400 Bad request");
+   }
+   else {
+      let book = {
+         id: request.body.id,
+         title: request.body.title,
+         details: request.body.details,
+      };
+      books.push(book);
+      console.log(`Added ${book.id}`)
+      response.status(200).send(books);
+   }
+
+});
+
+
+app.post("/books/:id/details", (request, response) => {
+   const body = request.body;
+      let details = {
+         author: body.author,
+         genre: body.genre,
+         year: body.year
+
+   };
+   books;
+   console.log(`Added ${book.id}`)
    response.status(200).send(books);
+   books.forEach(b => {
+      if (b.id === request.params.id) {
+         b.details.author = details.author;
+         b.details.genre = details.genre;
+         b.details.year = details.year;
+      }
+      response.send(books);
+   })
 
 })
+
+app.put("/books/:id", (request, response) => {
+
+   const body = request.body;
+   books.forEach(b => {
+      if (b.id === request.params.id) {
+         b.title = body.title;
+         b.details = body.details;
+      }
+      response.send(books);
+   })
+      
+});
+
+app.delete("/books/:id", (request, response) => {
+
+    books.forEach(b => {
+      if (b.id === request.params.id) {
+         var idx = books.indexOf(b);
+         if (idx > -1) {
+            books.splice(idx, 1);
+         }
+      }
+      response.send(books);
+   })
+});
+
+
+
+
+
 
 
 
